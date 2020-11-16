72c571599665ebb943db98dc6af8a5d5caa25b7f
Travis Lawson <travislawclimb@gmail.com>
2020-11-16 10:31:43 -0800

    Added CRUD functionality in Views folder for Customer and Employee.

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public int ZipCode { get; set; }
+
+        public int Balance { get; set; }
+
+        public string PickUpDay { get; set; }
+
+        public DateTime ExtraPickUpDate { get; set; }
+
+        // specify a START and END date to temporarily suspend my pickups
+        public DateTime SuspendPickUp { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+
+    }
+}
